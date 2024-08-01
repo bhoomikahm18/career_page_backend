1465c90b97cd441a97f0632ee678de9c0ec2e759
Bhoomika <bhoomikahm18@gmail.com>
2024-08-01 16:13:57 +0530

    Login function

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -28,3 +28,24 @@ module.exports.register = async (req, res) => {
 
     return res.status(200).json({ user });
 }
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email })
+    } catch (err) {
+        return console.log(err);
+    }
+
+    if (!existingUser) {
+        return res.json({ message: "User not found! Please Register" })
+    }
+
+    const user = new User({
+        email,
+        password
+    });
+
+    return res.status(200).json({ message: "Login Successfull" });
+}
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { register } = require('../controllers/user_controller');
+const { register, login } = require('../controllers/user_controller.js');
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
+userRouter.post("/login", login);
 
 module.exports = userRouter;
